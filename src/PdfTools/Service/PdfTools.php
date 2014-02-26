4df45ba914ed1e19a0a4045439300e6ff1463056
Guillaume Bouchez <guillaume.bouchez@infolegale.fr>
2014-02-26 16:40:44 +0100

    added some quick docblock

--- src/PdfTools/Service/PdfTools.php
+++ src/PdfTools/Service/PdfTools.php
@@ -10,6 +10,9 @@ namespace PdfTools\Service;
 
 class PdfTools extends \PdfTools\html2pdf\HTML2PDF implements \Zend\ServiceManager\ServiceLocatorAwareInterface
 {
+    /**
+     * @var \Zend\ServiceManager\ServiceLocatorInterface
+     */
     private $_serviceManager;
     
     public function __construct(\Zend\ServiceManager\ServiceLocatorInterface $serviceManager)
@@ -18,22 +21,35 @@ class PdfTools extends \PdfTools\html2pdf\HTML2PDF implements \Zend\ServiceManag
         call_user_func_array('parent::__construct', $config['html2pdf']);
     }
     
+    /**
+     * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
+     * @return \PdfTools\Service\PdfTools
+     */
     public function setServiceLocator(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator)
     {
         $this->_serviceManager = $serviceLocator;
         return $this;
     }
     
+    /**
+     * @return \Zend\ServiceManager\ServiceLocatorInterface
+     */
     public function getServiceLocator()
     {
         return $this->_serviceManager;
     }
     
+    /**
+     * @return \PdfTools\html2pdf\HTML2PDF_MyFpdf
+     */
     public function getPdfHandler()
     {
         return $this->pdf;
     }
     
+    /**
+     * @return string
+     */
     public function getPdfStream()
     {
         return $this->Output("file.pdf", "S");
